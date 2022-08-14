7bb4b488570cd980759879020315cdea038fb6b5
Svetlana <svetlanka2503@mail.ru>
2022-08-15 00:04:27 +0300

    Ta023

--- Task023/Program.cs
+++ Task023/Program.cs
@@ -6,9 +6,9 @@
 Console.WriteLine("Введите число: ");
 int N= Convert.ToInt32(Console.ReadLine());
 
-{    
+    
     for (int i = 1; i <= N; i++)
     {
        Console.WriteLine(i * i * i); 
     }
-}
+
